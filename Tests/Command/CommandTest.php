c90a3867029bf0e31e962ba2b4c98ac1c063805f
Yorkie Chadwick <ychadwick@gmail.com>
2016-02-05 16:44:22 +0100

    added empty test to implement

---
+++ Tests/Command/CommandTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the go-do-it  package.
+ *
+ * (c) net working AG <info@networking.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ibrows\BoxalinoBundle\Tests\Command;
+
+
+use Ibrows\BoxalinoBundle\Tests\Stub\KernelTestCase;
+
+class CommandTest extends KernelTestCase
+{
+
+    public function testFullExport(){
+
+    }
+
+    public function testPartialExport(){
+
+    }
+
+    public function testDeltaExport(){
+
+    }
+}
